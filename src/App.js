d5b40a0ef5b4346d449ce6b9f553cc8e8c9d3711
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-07 00:25:02 +0300

    Install konva canvas library

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Stage, Layer, Rect, Circle } from "react-konva";
+
 export default function App() {
-  return <div>app</div>;
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect width={50} height={50} fill="red" />
+        <Circle x={200} y={200} stroke="black" radius={50} />
+      </Layer>
+    </Stage>
+  );
 }
