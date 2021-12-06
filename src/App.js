ef52d5a12d0614896ae9ef316270f1311975f1b0
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-07 02:13:39 +0300

    Implement zoom functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { Stage, Layer, Rect, Circle } from "react-konva";
+import useWheelZoom from "./useWheelZoom";
 
 export default function App() {
+  const [stage, handleWheel] = useWheelZoom();
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      draggable={true}
+      onWheel={handleWheel}
+      scale={stage.scale}
+      x={stage.x}
+      y={stage.y}
+    >
       <Layer>
         <Rect width={50} height={50} fill="red" />
         <Circle x={200} y={200} stroke="black" radius={50} />
---
+++ src/useWheelZoom.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useWheelZoom() {
+  const [stage, setStage] = useState({
+    scale: { x: 1, y: 1 },
+    x: 0,
+    y: 0,
+  });
+
+  const handleWheel = (e) => {
+    e.evt.preventDefault();
+
+    const scaleBy = 1.1;
+    const stage = e.target.getStage();
+    const oldScale = stage.scaleX();
+    const mousePointTo = {
+      x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale,
+    };
+
+    const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    setStage({
+      scale: { x: newScale, y: newScale },
+      x: (stage.getPointerPosition().x / newScale - mousePointTo.x) * newScale,
+      y: (stage.getPointerPosition().y / newScale - mousePointTo.y) * newScale,
+    });
+  };
+
+  return [stage, handleWheel];
+}
